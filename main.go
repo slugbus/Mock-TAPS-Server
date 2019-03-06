0f185e06ca288cf2ca1a8a13ab970bc8fea8fb32
Jorge Henriquez <joahenri@ucsc.edu>
2019-03-05 21:04:03 -0800

    Remove wrong package info

--- main.go
+++ main.go
@@ -12,9 +12,6 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-// Package slugger is a go client library
-// for the UCSC TAPS API
-
 package main
 
 import (
